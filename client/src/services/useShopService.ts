63fbdad7d119b97cf20933690187125791946350
Brian <brianw.cho2002@gmail.com>
2022-11-08 23:37:46 -0500

    added interfaces and services

---
+++ client/src/services/useShopService.ts
@@ -0,0 +1,5 @@
+
+// wrapper hook for all Shop related API services
+export const useShopService = () => {
+  return {};
+};
---
+++ client/src/services/useUserService.ts
@@ -0,0 +1,5 @@
+
+// wrapper hook for all User related API services
+export const useUserService = () => {
+  return {};
+};
