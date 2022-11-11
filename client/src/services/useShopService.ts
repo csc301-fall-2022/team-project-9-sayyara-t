6768dd261271e5cbf2ff31420fbe5bca73f6b591
thelfenbaum <thelfenbaum@gmail.com>
2022-11-11 13:24:17 -0500

    updated shop services

--- client/src/services/useShopService.ts
+++ client/src/services/useShopService.ts
@@ -1,62 +1,140 @@
 import shopsData from '../assets/mock/shopData.json';
-import { Service, Shop, Time } from '../interfaces';
+import { Service, Shop, Time, RequestResult, User } from '../interfaces';
+import { useAPIService } from './useAPIService';
 
 // wrapper hook for all Shop related API services
 export const useShopService = () => {
+  const API_PATH = "/shopadmins"; //TODO: double check this is right
+  const apiService = useAPIService();
 
-  // TODO: replace mock API call to real API call
-  const getShopsForUser = async (userId: string): Promise<Array<Shop>> => {
-    return shopsData.map((shop) => {
-      return {
-        shopId: shop.shop_id,
-        name: shop.name,
-        address: shop.address,
-        phone: shop.phone,
-        email: shop.email,
-        description: shop.description,
-        time: shop.time as Time,
-        services: shop.services.map((service) => {
-          return {
-            serviceId: service.service_id,
-            shopId: service.shop_id,
-            name: service.name,
-            description: service.description,
-            price: service.price
-          } as Service;
-        })
-      } as Shop;
-    });
+  // TODO: test that effectively replaced mock API call
+  const getShopsForUser = async (userId: string): Promise<boolean> => {
+    const data = {
+      userId: userId
+    };
+
+    const result: RequestResult = await apiService.apiRequest(`${API_PATH}/user/:${userId}`, 'GET', data);
+
+    if (!result.success) {
+      const msg = result.data.message || "Unexpected Error";
+      return Promise.reject<boolean>(new Error(`Failed to get shops for this user: ${msg}`));
+    }
+
+    return result.success;
+
+    // return shopsData.map((shop) => {
+    //   return {
+    //     shopId: shop.shop_id,
+    //     name: shop.name,
+    //     address: shop.address,
+    //     phone: shop.phone,
+    //     email: shop.email,
+    //     description: shop.description,
+    //     time: shop.time as Time,
+    //     services: shop.services.map((service) => {
+    //       return {
+    //         serviceId: service.service_id,
+    //         shopId: service.shop_id,
+    //         name: service.name,
+    //         description: service.description,
+    //         price: service.price
+    //       } as Service;
+    //     })
+    //   } as Shop;
+    // });
   };
 
-  // TODO: replace mock API call to real API call
+
+  // TODO: test that effectively replaced mock API call
   const updateShop = async (shop: Shop): Promise<boolean> => {
-    console.log("Updating Shop");
-    console.log(shop);
+    const data = {
+      shopId: shop.shopId,
+      ownerIds: shop.ownerIds,
+      name: shop.name,
+      address: shop.address,
+      phone: shop.phone,
+      email: shop.email,
+      services: shop.services,
+      description: shop.description
+    };
 
-    const success = true;
+      console.log("Updating Shop");
+      console.log(shop);
 
-    return success ? success : Promise.reject<boolean>(new Error("Failed to updated shop"));
+      //const success = true;
+
+      //return success ? success : Promise.reject<boolean>(new Error("Failed to updated shop"));
+    
+      const result: RequestResult = await apiService.apiRequest(`${API_PATH}/${shop.shopId}`, 'PUT', data);
+
+      if (!result.success) {
+        const msg = result.data.message || "Unexpected Error";
+        return Promise.reject<boolean>(new Error(`Failed to update the shop with id: ${msg}`));
+      }
+
+      return result.success;
   };
 
-  // TODO: replace mock API call to real API call
-  const createShop = async (shop: Shop): Promise<string> => {
+  // TODO: test
+  const createShop = async (shop: Shop): Promise<boolean> => {
+    const data = {
+      shopId: shop.shopId,
+      ownerIds: shop.ownerIds,
+      name: shop.name,
+      address: shop.address,
+      phone: shop.phone,
+      email: shop.email,
+      services: shop.services,
+      description: shop.description
+    };
+
     console.log("Creating Shop");
     console.log(shop);
 
-    const success = true;
-    const shopId = Math.floor(Math.random() * 1000);
+    // const success = true;
+    // const shopId = Math.floor(Math.random() * 1000);
 
-    return success ? shopId.toString() : Promise.reject<string>(new Error("Failed to create shop"));
+    // return success ? shopId.toString() : Promise.reject<string>(new Error("Failed to create shop"));
+
+    const result: RequestResult = await apiService.apiRequest(`${API_PATH}/`, 'POST', data);
+
+      if (!result.success) {
+        const msg = result.data.message || "Unexpected Error";
+        return Promise.reject<boolean>(new Error(`Failed to create the requested shop: ${msg}`));
+      }
+
+      return result.success;
   };
   
   // TODO: replace mock API call to real API call
   const deleteShop = async (shop: Shop): Promise<boolean> => {
+    const data = {
+      shopId: shop.shopId,
+      ownerIds: shop.ownerIds,
+      name: shop.name,
+      address: shop.address,
+      phone: shop.phone,
+      email: shop.email,
+      services: shop.services,
+      description: shop.description
+    };
+    
     console.log("Deleting Shop");
     console.log(shop);
 
-    const success = true;
+    // const success = true;
 
-    return success ? success : Promise.reject<boolean>(new Error("Failed to delete shop"));
+    // return success ? success : Promise.reject<boolean>(new Error("Failed to delete shop"));
+    
+    const result: RequestResult = await apiService.apiRequest(`${API_PATH}/${shop.shopId}`, 'DELETE', data);
+
+      if (!result.success) {
+        const msg = result.data.message || "Unexpected Error";
+        return Promise.reject<boolean>(new Error(`Failed to delete the requested shop: ${msg}`));
+      }
+
+      return result.success;
+  
   };
 
   return {
