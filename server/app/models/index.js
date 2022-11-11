635f77cb5cd6b15715d3de64b6198825502b093b
longles <lingxiaoai@gmail.com>
2022-11-11 16:22:25 -0500

    fix for heroku import bug?

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -5,13 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = 'development';
 const config = require('../config/config.json')[env];
 const db = {};
 
 let sequelize;
 
 sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 
 fs
   .readdirSync(__dirname)
