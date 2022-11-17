e2358a4019e35271cc968b6503a0cabe815a17c4
longles <lingxiaoai@gmail.com>
2022-11-16 22:31:40 -0500

    finished GET and POST tests for shops

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const app = require('./server')
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
