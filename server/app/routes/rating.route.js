ad731cb15d7925bee33b88cbd5c89ce8abd5b2a4
Bradley-Mathi <bradleyskmathi@gmail.com>
2022-11-20 17:54:53 -0500

    Added appropriate verification to all service, rating, vehicle and shop routes that were not findOne or findAll routes. Also added some combined verifier functions to auth.jwt to allow for admins to view all things.

--- server/app/routes/rating.route.js
+++ server/app/routes/rating.route.js
@@ -1,13 +1,14 @@
+const {authJwt} = require("../middleware");
 module.exports = app =>{
     const rating = require ("../controllers/rating.controller.js");
     
     var router = require("express").Router();
 
-    router.post("/", rating.create);
+    router.post("/", [authJwt.verifyToken, authJwt.isVehicleOwnerOrAdmin], rating.create);
     router.get("/", rating.findAll);
     router.get("/:id", rating.findOne);
-    router.put("/:id", rating.update);
-    router.delete("/:id", rating.delete);
+    router.put("/:id", [authJwt.verifyToken, authJwt.isVehicleOwnerOrAdmin], rating.update);
+    router.delete("/:id", [authJwt.verifyToken, authJwt.isVehicleOwnerOrAdmin], rating.delete);
 
     app.use('/api/ratings', router);
 }
--- server/app/routes/vehicle.route.js
+++ server/app/routes/vehicle.route.js
@@ -1,13 +1,14 @@
+const {authJwt} = require("../middleware");
 module.exports = app =>{
     const vehicles = require ("../controllers/vehicle.controller.js");
     
     var router = require("express").Router();
 
-    router.post("/", vehicles.create);
+    router.post("/", [authJwt.verifyToken, authJwt.isVehicleOwnerOrAdmin], vehicles.create);
     router.get("/:id", vehicles.findOne);
-    router.put("/:id", vehicles.update);
-    router.delete("/:id", vehicles.delete);
-    router.get("/user/:user_id", vehicles.findAllByUserID);
+    router.put("/:id", [authJwt.verifyToken, authJwt.isVehicleOwnerOrAdmin], vehicles.update);
+    router.delete("/:id", [authJwt.verifyToken, authJwt.isVehicleOwnerOrAdmin], vehicles.delete);
+    router.get("/user/:user_id", [authJwt.verifyToken], vehicles.findAllByUserID);
 
     app.use('/api/vehicles', router); 
 }
