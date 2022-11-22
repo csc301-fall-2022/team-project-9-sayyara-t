e8cb31667b231dced72c46a153047c93c000f2e2
longles <lingxiaoai@gmail.com>
2022-11-21 19:00:45 -0500

    updated request search

--- server/app/seeders/20221120191336-demo-requests.js
+++ server/app/seeders/20221120191336-demo-requests.js
@@ -11,7 +11,7 @@ module.exports = {
         vehicle_id: '10000',
         quote_id: '100000',
         linked_request_id: null,
-        services: [],
+        services: ['abc1'],
         state: 2,
         description: 'Liquid constantly dripping from car',
         new_used: 3,
