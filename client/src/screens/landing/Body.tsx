bb6754e6dba87b67b3be9cfac4c37ede0e9064f4
Kevin <haozedeng@outlook.com>
2022-11-24 01:56:03 -0500

    got rid of distance/price

--- client/src/screens/landing/Body.tsx
+++ client/src/screens/landing/Body.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+// import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Button, Typography } from '@mui/material';
 import FormControl from '@mui/material/FormControl';
@@ -9,20 +9,20 @@ import FormLabel from '@mui/material/FormLabel';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
-import { useState } from 'react';
-import { Stack } from '@mui/system';
-import Slider from '@mui/material/Slider';
+// import { useState } from 'react';
+// import { Stack } from '@mui/system';
+// import Slider from '@mui/material/Slider';
 import { ShopTile } from './ShopTile';
 import { Shop } from '../../interfaces';
 
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//     ...theme.typography.body2,
+//     padding: theme.spacing(1),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+// }));
 
 interface BodyProps {
     shops: Array<Shop>,
@@ -31,47 +31,47 @@ interface BodyProps {
 }
 
 export const Body = ({ shops, sort, setSort }: BodyProps) => {
-    const [price, setPrice] = useState(1);
+    // const [price, setPrice] = useState(1);
 
-    const marks = [
-        {
-          value: 6,
-          label: 'Min',
-        },
-        {
-          value: 7,
-          label: '7 km',
-        },
-        {
-          value: 8,
-          label: '8 km',
-        },
-        {
-          value: 9,
-          label: '9 km',
-        },
-        {
-            value: 10,
-            label: 'Max',
-        },
-    ];
-    
+    // const marks = [
+    //     {
+    //         value: 6,
+    //         label: 'Min',
+    //     },
+    //     {
+    //         value: 7,
+    //         label: '7 km',
+    //     },
+    //     {
+    //         value: 8,
+    //         label: '8 km',
+    //     },
+    //     {
+    //         value: 9,
+    //         label: '9 km',
+    //     },
+    //     {
+    //         value: 10,
+    //         label: 'Max',
+    //     },
+    // ];
+
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSort(event.target.value);
     };
 
-    const handleClick = (price: number) => {
-        setPrice(price);
-    };
+    // const handleClick = (price: number) => {
+    //     setPrice(price);
+    // };
 
-    function valuetext(value: number) {
-        return `${value} km`;
-    }
+    // function valuetext(value: number) {
+    //     return `${value} km`;
+    // }
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-            <Grid item xs={3}>
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
                     <Typography
                         variant="h4"
                         component="div"
@@ -82,41 +82,41 @@ export const Body = ({ shops, sort, setSort }: BodyProps) => {
                             p: 5,
                             fontWeight: 'bold'
                         }}
-                        >
+                    >
                         Filters
                     </Typography>
 
-                <FormControl
-                sx={{
-                    textAlign: 'left',
-                    mx: 8,
-                    p: 5,
-                    fontWeight: 'bold'
-                }}
-                >
-                    <FormLabel id='filter-label'>
-                    <Typography
-                        variant="h5"
-                        component="div"
-                        color="black"
+                    <FormControl
                         sx={{
+                            textAlign: 'left',
+                            mx: 8,
+                            p: 5,
                             fontWeight: 'bold'
                         }}
-                        >
-                        Sort
-                    </Typography>
-                    </FormLabel>
-                    <RadioGroup 
-                        name='filter'
-                        aria-labelledby='filter-label'
-                        value={sort}
-                        onChange={handleChange}
                     >
-                        <FormControlLabel control={<Radio />} label='Price' value='price'/>
-                        <FormControlLabel control={<Radio />} label='Ratings' value='star'/>
-                    </RadioGroup>
-                </FormControl>
-                <FormControl
+                        <FormLabel id='filter-label'>
+                            <Typography
+                                variant="h5"
+                                component="div"
+                                color="black"
+                                sx={{
+                                    fontWeight: 'bold'
+                                }}
+                            >
+                                Sort
+                            </Typography>
+                        </FormLabel>
+                        <RadioGroup
+                            name='filter'
+                            aria-labelledby='filter-label'
+                            value={sort}
+                            onChange={handleChange}
+                        >
+                            <FormControlLabel control={<Radio />} label='Price' value='price' />
+                            <FormControlLabel control={<Radio />} label='Ratings' value='star' />
+                        </RadioGroup>
+                    </FormControl>
+                    {/* <FormControl
                 sx={{
                     textAlign: 'left',
                     fontWeight: 'bold',
@@ -141,58 +141,58 @@ export const Body = ({ shops, sort, setSort }: BodyProps) => {
                         <Button variant='contained' color='secondary' sx={{ borderRadius: 8 }} onClick={() => handleClick(3)}>$$$</Button>
                         <Button variant='contained' color='secondary' sx={{ borderRadius: 8 }} onClick={() => handleClick(4)}>$$$$</Button>
                     </Stack>
-                </FormControl>
+                </FormControl> */}
 
-                <FormControl
-                sx={{
-                    textAlign: 'left',
-                    fontWeight: 'bold',
-                    mx: 8,
-                    p: 5
-                }}
-                >
-                    <FormLabel id='filter-label'>
-                    <Typography
-                        variant="h5"
-                        component="div"
-                        color="black"
+                    {/* <FormControl
                         sx={{
-                            fontWeight: 'bold'
+                            textAlign: 'left',
+                            fontWeight: 'bold',
+                            mx: 8,
+                            p: 5
                         }}
                     >
-                        Distance
-                    </Typography>
-                    </FormLabel>
-                    <Box sx={{ width: 300 }}>
-                    <Slider
-                        aria-label="distance-filter"
-                        defaultValue={6}
-                        getAriaValueText={valuetext}
-                        step={1}
-                        marks={marks}
-                        max={10}
-                        min={6}
-                    />
-                    </Box>
-                </FormControl>
-            </Grid>
-            <Grid item xs={9}>
-                <Grid 
-                    container
-                    spacing={10}
-                    sx={{
-                        itemAlign: 'center',
-                        p: 10
-                    }}
-                >
-                    {shops.map((shop) => (
-                        <Grid item width={350} key={shop.shopId}>
-                            <ShopTile name={shop.name} id={shop.shopId}></ShopTile>
-                        </Grid>
-                    ))}
+                        <FormLabel id='filter-label'>
+                            <Typography
+                                variant="h5"
+                                component="div"
+                                color="black"
+                                sx={{
+                                    fontWeight: 'bold'
+                                }}
+                            >
+                                Distance
+                            </Typography>
+                        </FormLabel>
+                        <Box sx={{ width: 300 }}>
+                            <Slider
+                                aria-label="distance-filter"
+                                defaultValue={6}
+                                getAriaValueText={valuetext}
+                                step={1}
+                                marks={marks}
+                                max={10}
+                                min={6}
+                            />
+                        </Box>
+                    </FormControl> */}
+                </Grid>
+                <Grid item xs={9}>
+                    <Grid
+                        container
+                        spacing={10}
+                        sx={{
+                            itemAlign: 'center',
+                            p: 10
+                        }}
+                    >
+                        {shops.map((shop) => (
+                            <Grid item width={350} key={shop.shopId}>
+                                <ShopTile name={shop.name} id={shop.shopId}></ShopTile>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Grid>
             </Grid>
-        </Grid>
-    </Box>
-  );
+        </Box>
+    );
 };
