230a31269cf97a7f36e7a92729659d4562facb25
Kevin <haozedeng@outlook.com>
2022-11-24 01:23:04 -0500

    rating + signup fixes

--- client/src/screens/shop/ShopInfo.tsx
+++ client/src/screens/shop/ShopInfo.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from '@mui/material/styles';
-import { 
+import {
     Box,
     Grid,
     Paper,
     Stack,
-    Typography
+    Typography,
+    Rating
 } from '@mui/material';
 import { ServiceInfo } from './ServiceInfo';
 import { Service, Shop } from '../../interfaces';
-
+import { useRatingService } from '../../services/useRatingService';
 
 const Item = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(1),
@@ -25,68 +26,83 @@ interface ShopInfoProps {
 // TODO: Finish this tomorrow using the layout in Auto-layout section of Grid
 // in combination with 
 export const ShopInfo = ({ shop, services }: ShopInfoProps) => {
-  return (
-    <Box>
-        <Box 
-            sx={{ 
-                flexGrow: 1,
-                py: 5,
-                px: 20
-            }}
-        >
-        <Grid container spacing={3}>
-            <Grid item xs>
-                <Item>
-                <Typography
-                        variant="h5"
-                        component="div"
-                        sx={{
-                            fontWeight: "bold"
-                        }}
-                    >
-                        {shop.name}
-                    </Typography>
-                </Item>
-            </Grid>
-            <Grid item xs>
-                <Item>Rating: N/A</Item>
-            </Grid>
-            <Grid item xs>
-                <Item>Price Range: </Item>
-            </Grid>
-            <Grid item xs>
-                <Item>{`Open Hours: ${shop.time ? shop.time.start : ""}-${shop.time ? shop.time.end : ""}`}</Item>
-            </Grid>
-        </Grid>
+    const [rating, setRating] = useState<number | null>(0);
+    const [price, setPrice] = useState<number | null>(0);
+    const ratingService = useRatingService();
+    useEffect(() => {
+        const loadData = async () => {
+            await ratingService.getShopRating(shop).then((_rating) => setRating(_rating));
+            await ratingService.getShopPrice(shop).then((_price) => setPrice(_price));
+        };
+
+        loadData();
+    }, [shop]);
+
+
+    return (
+        <Box>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    py: 5,
+                    px: 20
+                }}
+            >
+                <Grid container spacing={3}>
+                    <Grid item xs>
+                        <Item>
+                            <Typography
+                                variant="h5"
+                                component="div"
+                                sx={{
+                                    fontWeight: "bold"
+                                }}
+                            >
+                                {shop.name}
+                            </Typography>
+                        </Item>
+                    </Grid>
+                    <Grid item xs>
+                        <Item>
+                            <Rating name="shop-rating" value={rating} precision={0.5} readOnly />
+                        </Item>
+                    </Grid>
+                    <Grid item xs>
+                        <Item>Price Range: ${price}</Item>
+                    </Grid>
+                    <Grid item xs>
+                        <Item>{`Open Hours: ${shop.time ? shop.time.start : ""}-${shop.time ? shop.time.end : ""}`}</Item>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Typography
+                variant="h4"
+                component="div"
+                sx={{
+                    textAlign: 'left',
+                    mx: 20,
+                    fontWeight: "bold",
+                    flexGrow: 1
+                }}
+            >
+                Service List
+            </Typography>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    py: 5,
+                    px: 20
+                }}
+            >
+                <Stack spacing={5} direction="column">
+                    {services.length > 0 && services.map((service) => (
+                        <ServiceInfo
+                            key={service.serviceId}
+                            service={service}
+                        />
+                    ))}
+                </Stack>
+            </Box>
         </Box>
-        <Typography
-            variant="h4"
-            component="div"
-            sx={{
-                textAlign: 'left',
-                mx: 20,
-                fontWeight: "bold",
-                flexGrow: 1
-            }}
-        >
-            Service List
-        </Typography>
-        <Box 
-            sx={{ 
-                flexGrow: 1,
-                py: 5,
-                px: 20
-            }}
-        >
-            <Stack spacing={5} direction="column">
-                {services.length > 0 && services.map((service) => (
-                    <ServiceInfo 
-                        key={service.serviceId}
-                        service={service}
-                    />
-                ))}
-            </Stack>
-        </Box>
-    </Box>
-  );
+    );
 };
